86e1104e28f7ee12f65eaa9738a2c2a5b4df1c1c
HugoSwen <2407475450@qq.com>
2023-07-14 11:49:55 +0800

    optimize

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pickle
 import tempfile
 import threading
 import time
+import uuid
 
 import joblib
 import pymysql
@@ -37,6 +38,11 @@ def checkfile():
     else:
         return False
     return True
+
+
+@app.route("/")
+def hello():
+    return hello
 
 
 # login path
@@ -92,17 +98,20 @@ def train():
     file = request.files['file']
     dlrfc.train(file)
 
-    # cursor, cnn = getcurcor()
-    # username = session.get("username")
-    # sql = "update user set model_blob = %s where username = %s"
-    # try:
-    #     cursor.execute(sql, (pickle.dumps(dlrfc), username))
-    #     cnn.commit()
-    # except pymysql.Error as e:
-    #     cnn.rollback()
-    #     print(e)
-    # cursor.close()
-    # cnn.close()
+    username = session.get("username")
+    file_path = "modelfiles/" + username + '.joblib'
+    joblib.dump(dlrfc, file_path)
+
+    cursor, cnn = getcurcor()
+    sql = "update user set model_filepath = %s where username = %s"
+    try:
+        cursor.execute(sql, (file_path, username))
+        cnn.commit()
+    except pymysql.Error as e:
+        cnn.rollback()
+        print(e)
+    cursor.close()
+    cnn.close()
 
     return 'success'
 
@@ -126,18 +135,18 @@ def test():
     if not checkfile():
         return "failure"
 
-    # cursor, cnn = getcurcor()
-    # username = session.get("username")
-    # sql = "select * from user where username = %s"
-    # cursor.execute(sql, username)
-    # row = cursor.fetchone()
-    # cursor.close()
-    # cnn.close()
-    #
-    # if row[3]:
-    #     dlrfc = pickle.loads(row[3])
-    # else:
-    #     return "failure"
+    cursor, cnn = getcurcor()
+    username = session.get("username")
+    sql = "select * from user where username = %s"
+    cursor.execute(sql, username)
+    row = cursor.fetchone()
+    cursor.close()
+    cnn.close()
+
+    if row[3]:
+        dlrfc = joblib.load(row[3])
+    else:
+        return "failure"
 
     file = request.files['file']
     result = dlrfc.test(file)
@@ -149,20 +158,15 @@ def test():
 # download path
 @app.route('/download', methods=['post'])
 def download():
-    # cursor, cnn = getcurcor()
-    # username = session.get("username")
-    # sql = "select * from user where username = %s"
-    # cursor.execute(sql, username)
-    # row = cursor.fetchone()
+    cursor, cnn = getcurcor()
+    username = session.get("username")
+    sql = "select * from user where username = %s"
+    cursor.execute(sql, username)
+    row = cursor.fetchone()
 
-    if dlrfc.selection_model:
-        with tempfile.NamedTemporaryFile(suffix='.joblib', delete=False, dir="tempfiles") as temp_file:
-            file_path = temp_file.name
-            joblib.dump(dlrfc, file_path)
-        #     print(file_path)
-        # response = make_response(send_file(file_path, as_attachment=True))
-        response = Response(pickle.dumps(dlrfc), mimetype='application/octet-stream')
-
+    if row[3]:
+        response = make_response(send_file(row[3], as_attachment=True))
+        # # response = Response(pickle.dumps(dlrfc), mimetype='application/octet-stream')
         response.headers.set('Content-Disposition', 'attachment', filename='model.joblib')
 
         # def delete_file(file_path):
@@ -176,18 +180,6 @@ def download():
         return response
     else:
         return "failure"
-
-
-# upload path
-@app.route('/upload', methods=['post'])
-def upload():
-    if not checkfile():
-        return "failure"
-
-    file = request.files['file']
-    global dlrfc
-    dlrfc = joblib.load(file)
-    return "success"
 
 
 if __name__ == '__main__':
